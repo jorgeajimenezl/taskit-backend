9448972d844baa49040dfb42ddab6a53903a5f91
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-07-12 15:21:28 +0200

    feat: enhance AppTask entity with additional properties and add TaskComment entity for task discussions

--- taskit-backend/Taskit.Domain/Entities/AppTask.cs
+++ taskit-backend/Taskit.Domain/Entities/AppTask.cs
@@ -11,13 +11,25 @@ public class AppTask : BaseEntity
     public DateTime? DueDate { get; set; }
     public DateTime? CompletedAt { get; set; }
     public Enums.TaskStatus Status { get; set; } = Enums.TaskStatus.Created;
+    public int Priority { get; set; } = 0;
+    public bool IsArchived { get; set; } = false;
+    public int Complexity { get; set; } = 0;
+    public int CompletedPercentage { get; set; } = 0;
+
     public string? AuthorId { get; set; }
     public AppUser? Author { get; set; }
+
     public string? AssignedUserId { get; set; }
     public AppUser? AssignedUser { get; set; }
+
     public int? ProjectId { get; set; }
     public Project? Project { get; set; }
+
     public int? ParentTaskId { get; set; }
     public AppTask? ParentTask { get; set; }
-    public ICollection<AppTask> SubTasks { get; set; } = new List<AppTask>();
+
+    // Navigation properties for related entities
+    public ICollection<AppTask> SubTasks { get; set; } = [];
+    public ICollection<TaskTag> Tags { get; set; } = [];
+    public ICollection<TaskComment> Comments { get; set; } = [];
 }
---
+++ taskit-backend/Taskit.Domain/Entities/TaskComment.cs
@@ -0,0 +1,12 @@
+namespace Taskit.Domain.Entities;
+
+public class TaskComment : BaseEntity
+{
+    public required string Content { get; set; }
+
+    public required string TaskId { get; set; }
+    public required AppTask Task { get; set; }
+
+    public required string AuthorId { get; set; }
+    public required AppUser Author { get; set; }
+}
