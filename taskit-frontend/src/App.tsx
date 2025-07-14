7a47d196db4a37561bf03a91379975493dc3eb77
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-07-14 10:53:42 +0200

    feat: implement routing with AppRoutes component and add Login page

--- taskit-frontend/src/App.tsx
+++ taskit-frontend/src/App.tsx
@@ -1,14 +1,11 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
 import './App.css'
+import Router from './routes/AppRoutes'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <>
+      <Router />
+    </> 
   )
 }
 
