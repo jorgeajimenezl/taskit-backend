41990ee48d7ae7b77a1ea302cf04bb93ecdde7c1
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-07-16 22:20:46 +0200

    feat: implement media service

--- src/Taskit.Application/Services/MediaService.cs
+++ src/Taskit.Application/Services/MediaService.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Taskit.Application.Interfaces;
 using Taskit.Domain.Entities;
 
@@ -6,4 +7,55 @@ namespace Taskit.Application.Services;
 public class MediaService(IMediaRepository mediaRepository)
 {
     private readonly IMediaRepository _mediaRepository = mediaRepository;
+
+    public Task AddAsync(Media media)
+    {
+        return _mediaRepository.AddAsync(media);
+    }
+
+    public Task<Media?> GetByIdAsync(int id)
+    {
+        return _mediaRepository.GetByIdAsync(id);
+    }
+
+    public async Task<IEnumerable<Media>> GetMediaAsync(string modelType, int modelId, string? collectionName = null)
+    {
+        var query = _mediaRepository.Query()
+            .Where(m => m.ModelType == modelType && m.ModelId == modelId);
+        if (!string.IsNullOrEmpty(collectionName))
+            query = query.Where(m => m.CollectionName == collectionName);
+        return await query.ToListAsync();
+    }
+
+    public async Task<Media?> GetFirstMediaAsync(string modelType, int modelId, string? collectionName = null)
+    {
+        var query = _mediaRepository.Query()
+            .Where(m => m.ModelType == modelType && m.ModelId == modelId);
+        if (!string.IsNullOrEmpty(collectionName))
+            query = query.Where(m => m.CollectionName == collectionName);
+        return await query.FirstOrDefaultAsync();
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        if (!await _mediaRepository.ExistsAsync(id))
+            return false;
+
+        await _mediaRepository.DeleteAsync(id);
+        return true;
+    }
+
+    public async Task ClearMediaCollectionAsync(string modelType, int modelId, string collectionName)
+    {
+        var mediaItems = await _mediaRepository.Query()
+            .Where(m => m.ModelType == modelType &&
+                        m.ModelId == modelId &&
+                        m.CollectionName == collectionName)
+            .ToListAsync();
+
+        foreach (var media in mediaItems)
+        {
+            await _mediaRepository.DeleteAsync(media.Id);
+        }
+    }
 }
