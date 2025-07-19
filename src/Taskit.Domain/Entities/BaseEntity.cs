6042a02e5723d078190805ac491917cc09b763b2
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-07-19 21:17:08 +0200

    fix(entity): remove required modifier from Id property in BaseEntity and RefreshToken

--- src/Taskit.Domain/Entities/BaseEntity.cs
+++ src/Taskit.Domain/Entities/BaseEntity.cs
@@ -7,7 +7,7 @@ namespace Taskit.Domain.Entities;
 public abstract class BaseEntity<TKey> : IEntity<TKey>
 {
     [Key]
-    public required TKey Id { get; set; }
+    public TKey Id { get; set; } = default!;
 
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
