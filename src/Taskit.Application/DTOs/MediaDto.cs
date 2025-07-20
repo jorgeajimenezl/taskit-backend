d89a29936d5f91952aaf35179d556fb4f0042d64
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-07-20 12:34:14 +0200

    feat(media): validate uploads

---
+++ src/Taskit.Application/DTOs/MediaDto.cs
@@ -0,0 +1,20 @@
+using AutoMapper;
+using Taskit.Domain.Entities;
+
+namespace Taskit.Application.DTOs;
+
+public record MediaDto
+{
+    public int Id { get; init; }
+    public required string Url { get; init; }
+
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<Media, MediaDto>()
+                .ForMember(d => d.Url, opt => opt.MapFrom(src => $"/uploads/{src.FileName}"));
+        }
+    }
+}
+
