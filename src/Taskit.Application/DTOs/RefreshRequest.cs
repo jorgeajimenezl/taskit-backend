557b3530ede145edadfbd416bca4d047844fde6f
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-07-21 17:19:35 +0200

    Update RefreshRequest.cs

--- src/Taskit.Application/DTOs/RefreshRequest.cs
+++ src/Taskit.Application/DTOs/RefreshRequest.cs
@@ -1,9 +1,6 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace Taskit.Application.DTOs;
 
 public record RefreshRequest
 {
-    [Required]
-    public required string RefreshToken { get; init; }
+    public string? RefreshToken { get; init; }
 }
