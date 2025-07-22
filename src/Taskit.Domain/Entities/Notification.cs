ff62c00b6200d14e9087c8837d5dece40c8228d2
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-07-22 13:13:52 +0200

    feat(notification): add Notification entity and NotificationType enum

---
+++ src/Taskit.Domain/Entities/Notification.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Taskit.Domain.Enums;
+
+namespace Taskit.Domain.Entities
+{
+    public class Notification : BaseEntity<int>
+    {
+        [Required, MaxLength(100)]
+        public required string Title { get; set; }
+
+        [MaxLength(500)]
+        public string? Message { get; set; }
+
+        public NotificationType Type { get; set; }
+        public bool IsRead { get; set; } = false;
+        public IDictionary<string, object?>? Data { get; set; }
+
+        [Required, ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+        public required AppUser User { get; set; }
+    }
+}
