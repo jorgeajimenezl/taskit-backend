527fc0e677a9c7351bdfb66131ecc7fd7ea9e4f6
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-07-23 11:55:34 +0200

    refactor(notification): streamline Program.cs by consolidating service configuration

--- src/Taskit.Notification.Worker/Program.cs
+++ src/Taskit.Notification.Worker/Program.cs
@@ -3,27 +3,23 @@ using MassTransit;
 using Taskit.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
-var builder = Host.CreateDefaultBuilder(args);
-
-builder.ConfigureServices((hostContext, services) =>
-{
-    services.AddDbContext<AppDbContext>(options =>
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
     {
-        var connectionString = hostContext.Configuration.GetConnectionString("DefaultConnection");
-        options.UseSqlite(connectionString);
-    });
+        var connectionString = context.Configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<AppDbContext>(options => options.UseSqlite(connectionString));
 
-    services.AddMassTransit(x =>
-    {
-        var entryAssembly = Assembly.GetEntryAssembly();
-        x.AddConsumers(entryAssembly);
-
-        x.UsingInMemory((context, cfg) =>
+        services.AddMassTransit(x =>
         {
-            cfg.ConfigureEndpoints(context);
-        });
-    });
-});
+            var entryAssembly = Assembly.GetEntryAssembly();
+            x.AddConsumers(entryAssembly);
 
-var host = builder.Build();
+            x.UsingInMemory((context, cfg) =>
+            {
+                cfg.ConfigureEndpoints(context);
+            });
+        });
+    })
+    .Build();
+
 host.Run();
