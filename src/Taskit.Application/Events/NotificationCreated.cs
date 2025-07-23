bbbbdc674e76d9233240c5df0a7929526b4fa4b6
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-07-23 11:17:18 +0200

    feat(notification): enhance NotificationService to publish notifications and add NotificationCreated event

---
+++ src/Taskit.Application/Events/NotificationCreated.cs
@@ -0,0 +1,13 @@
+using Taskit.Domain.Enums;
+
+namespace Taskit.Application.Events;
+
+public record NotificationCreated
+(
+    int NotificationId,
+    string UserId,
+    string Title,
+    string? Message,
+    NotificationType Type,
+    IDictionary<string, object?>? Data
+);
--- src/Taskit.Application/Services/NotificationService.cs
+++ src/Taskit.Application/Services/NotificationService.cs
@@ -1,14 +1,17 @@
+using MassTransit;
+using Taskit.Application.Events;
 using Taskit.Application.Interfaces;
 using Taskit.Domain.Entities;
 using Taskit.Domain.Enums;
 
 namespace Taskit.Application.Services;
 
-public class NotificationService(INotificationRepository notificationRepository)
+public class NotificationService(INotificationRepository notificationRepository, IPublishEndpoint publisher)
 {
     private readonly INotificationRepository _notificationRepository = notificationRepository;
+    private readonly IPublishEndpoint _publisher = publisher;
 
-    public async Task AddNotificationAsync(string userId, string title, NotificationType type, string? message = null, IDictionary<string, object?>? data = null)
+    public async Task CreateAsync(string userId, string title, NotificationType type, string? message = null, IDictionary<string, object?>? data = null)
     {
         var notification = new Notification
         {
@@ -19,6 +22,8 @@ public class NotificationService(INotificationRepository notificationRepository)
             UserId = userId
         };
 
-        await _notificationRepository.AddAsync(notification);
+        await _notificationRepository.AddAsync(notification, saveChanges: false);
+        await _publisher.Publish(new NotificationCreated(notification.Id, userId, title, message, type, data));
+        await _notificationRepository.SaveChangesAsync();
     }
 }
