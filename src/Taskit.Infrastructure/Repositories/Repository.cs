7e0607d08231fc6b8766bff19797a27ebe329aea
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-07-23 11:13:51 +0200

    refactor(repository): add saveChanges parameter to repository methods

--- src/Taskit.Infrastructure/Repositories/Repository.cs
+++ src/Taskit.Infrastructure/Repositories/Repository.cs
@@ -24,32 +24,36 @@ public class Repository<TEntity, TKey> : IRepository<TEntity, TKey> where TEntit
         return await _dbSet.ToListAsync();
     }
 
-    public virtual async Task AddAsync(TEntity entity)
+    public virtual async Task AddAsync(TEntity entity, bool saveChanges = true)
     {
         await _dbSet.AddAsync(entity);
-        await _context.SaveChangesAsync();
+        if (saveChanges)
+            await _context.SaveChangesAsync();
     }
 
-    public virtual async Task UpdateAsync(TEntity entity)
+    public virtual async Task UpdateAsync(TEntity entity, bool saveChanges = true)
     {
         _dbSet.Update(entity);
-        await _context.SaveChangesAsync();
+        if (saveChanges)
+            await _context.SaveChangesAsync();
     }
 
-    public virtual async Task DeleteAsync(TKey id)
+    public virtual async Task DeleteAsync(TKey id, bool saveChanges = true)
     {
         var entity = await GetByIdAsync(id);
         if (entity != null)
         {
             _dbSet.Remove(entity);
-            await _context.SaveChangesAsync();
+            if (saveChanges)
+                await _context.SaveChangesAsync();
         }
     }
 
-    public virtual async Task DeleteRangeAsync(IEnumerable<TEntity> entities)
+    public virtual async Task DeleteRangeAsync(IEnumerable<TEntity> entities, bool saveChanges = true)
     {
         _dbSet.RemoveRange(entities);
-        await _context.SaveChangesAsync();
+        if (saveChanges)
+            await _context.SaveChangesAsync();
     }
 
     public virtual async Task<bool> ExistsAsync(TKey id)
@@ -61,5 +65,10 @@ public class Repository<TEntity, TKey> : IRepository<TEntity, TKey> where TEntit
     public virtual IQueryable<TEntity> Query()
     {
         return _dbSet.AsQueryable();
+    }
+
+    public Task<int> SaveChangesAsync()
+    {
+        return _context.SaveChangesAsync();
     }
 }
