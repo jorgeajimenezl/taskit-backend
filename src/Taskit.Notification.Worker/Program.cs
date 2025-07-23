426426c929b108d8a73e709a8a4d4bff40679e28
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-07-23 11:50:29 +0200

    refactor(notification): clean up unused usings and MassTransit configuration

--- src/Taskit.Notification.Worker/Program.cs
+++ src/Taskit.Notification.Worker/Program.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Hosting;
 using MassTransit;
 using Taskit.Infrastructure;
 using Microsoft.EntityFrameworkCore;
@@ -20,16 +15,8 @@ builder.ConfigureServices((hostContext, services) =>
 
     services.AddMassTransit(x =>
     {
-        x.SetKebabCaseEndpointNameFormatter();
-
-        x.SetInMemorySagaRepositoryProvider();
-
         var entryAssembly = Assembly.GetEntryAssembly();
-
         x.AddConsumers(entryAssembly);
-        x.AddSagaStateMachines(entryAssembly);
-        x.AddSagas(entryAssembly);
-        x.AddActivities(entryAssembly);
 
         x.UsingInMemory((context, cfg) =>
         {
