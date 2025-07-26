24c271a8138f4984e6e1d25cf413557040fc093d
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-07-26 11:06:25 +0200

    feat: implement OAuthController for external authentication handling

---
+++ src/Taskit.Web/Controllers/OAuthController.cs
@@ -0,0 +1,67 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using OpenIddict.Client.AspNetCore;
+using Taskit.Application.Common.Exceptions;
+using Taskit.Application.Services;
+
+namespace Taskit.Web.Controllers;
+
+public class OAuthController(AuthService authService) : ApiControllerBase
+{
+    private readonly AuthService _authService = authService;
+    private static Dictionary<string, string> ProvidersMapping = new Dictionary<string, string>
+    {
+        ["github"] = "GitHub",
+    };
+
+    private static string GetProviderName(string provider)
+    {
+        if (ProvidersMapping.TryGetValue(provider.ToLowerInvariant(), out var providerName))
+            return providerName;
+
+        throw new ValidationException(new Dictionary<string, string[]>
+        {
+            ["provider"] = ["Invalid provider specified."]
+        });
+    }
+
+    [HttpGet("login/{provider}")]
+    public IActionResult Login(string provider)
+    {
+        var providerName = GetProviderName(provider);
+
+        var properties = new AuthenticationProperties(new Dictionary<string, string?>
+        {
+            [OpenIddictClientAspNetCoreConstants.Properties.ProviderName] = providerName,
+        })
+        {
+            RedirectUri = Url.Action(nameof(ExternalLoginCallback), providerName)
+        };
+
+        return Challenge(properties, OpenIddictClientAspNetCoreDefaults.AuthenticationScheme);
+    }
+
+    [HttpGet("callback/{provider}")]
+    public async Task<IActionResult> ExternalLoginCallback(string provider)
+    {
+        var result = await HttpContext.AuthenticateAsync(OpenIddictClientAspNetCoreDefaults.AuthenticationScheme);
+
+        if (!result.Succeeded || result?.Principal is null)
+            // TODO: create a custom exception for authentication failures
+            return BadRequest("Authentication failed.");
+
+        if (result?.Principal is not ClaimsPrincipal principal)
+            return BadRequest();
+
+        var providerUserId = principal.FindFirst(ClaimTypes.NameIdentifier)?.Value
+            ?? principal.FindFirst("sub")?.Value;
+
+        if (string.IsNullOrEmpty(providerUserId))
+            return BadRequest();
+
+        var providerName = GetProviderName(provider);
+        var loginResponse = await _authService.ExternalLoginAsync(providerName, providerUserId);
+        return Ok(loginResponse);
+    }
+}
