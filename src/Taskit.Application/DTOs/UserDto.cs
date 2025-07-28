a38f0136451ec074c79daee2731799d08db4fdaf
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-07-28 11:51:52 +0200

    feat: add missing [Required] attributes to UserDto properties

--- src/Taskit.Application/DTOs/UserDto.cs
+++ src/Taskit.Application/DTOs/UserDto.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Taskit.Application.DTOs;
 
 public record UserDto
 {
+    [Required]
     public required string UserName { get; init; }
+
+    [Required]
     public required string Email { get; init; }
+
+    [Required]
     public required string FullName { get; init; }
 }
