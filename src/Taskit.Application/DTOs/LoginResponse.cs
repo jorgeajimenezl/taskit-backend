08fbc766c93027b9c5ca2b6c68a9f741a5e75e4a
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-07-31 19:11:58 +0200

    fix(auth): make UserProfileDto required in LoginResponse

--- src/Taskit.Application/DTOs/LoginResponse.cs
+++ src/Taskit.Application/DTOs/LoginResponse.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Taskit.Application.DTOs;
 
 public record LoginResponse
 {
     public string? AccessToken { get; init; }
     public string? RefreshToken { get; init; }
-    public UserProfileDto? User { get; init; }
+
+    [Required]
+    public required UserProfileDto User { get; init; }
 }
 
