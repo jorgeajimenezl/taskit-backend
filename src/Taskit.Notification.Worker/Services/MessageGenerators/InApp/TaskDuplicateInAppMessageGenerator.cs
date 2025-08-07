cd2e01bcc8b027d92c693381592e18c411a4771c
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-08-07 23:23:03 +0200

    feat: add TaskDuplicateInAppMessageGenerator and TaskDuplicateRecipientResolver for handling duplicate task notifications

--- src/Taskit.Notification.Worker/Services/MessageGenerators/InApp/TaskDuplicateInAppMessageGenerator.cs
+++ src/Taskit.Notification.Worker/Services/MessageGenerators/InApp/TaskDuplicateInAppMessageGenerator.cs
@@ -1,10 +1,6 @@
-using Microsoft.Extensions.Options;
-using MimeKit;
-using Taskit.Domain.Enums;
 using Taskit.Domain.Events;
 using Taskit.Notification.Worker.Common;
 using Taskit.Notification.Worker.Interfaces;
-using Taskit.Notification.Worker.Settings;
 
 namespace Taskit.Notification.Worker.Services.MessageGenerators.Email;
 
