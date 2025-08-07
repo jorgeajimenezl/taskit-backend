c42276eacf77388c90e32dad73c6cb42edc527c0
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-08-07 09:01:14 +0200

    refactor: simplify ProjectRole reference in ProjectMember entity

--- src/Taskit.Domain/Entities/ProjectMember.cs
+++ src/Taskit.Domain/Entities/ProjectMember.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Taskit.Domain.Enums;
 
 namespace Taskit.Domain.Entities;
 
@@ -14,5 +15,5 @@ public class ProjectMember : BaseEntity<int>
     public AppUser? User { get; set; }
 
     [Required]
-    public required Enums.ProjectRole Role { get; set; } = Enums.ProjectRole.Member;
+    public required ProjectRole Role { get; set; } = ProjectRole.Member;
 }
