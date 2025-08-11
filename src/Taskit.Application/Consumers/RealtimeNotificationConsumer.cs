654dfa528123dcbb7b3c5cebdf01199061ed9dd6
Jorge Alejandro Jiménez Luna <jorgeajimenezl17@gmail.com>
2025-08-11 12:20:14 +0200

    feat: implement logging in RealtimeNotificationConsumer for NotificationCreated events

--- src/Taskit.Application/Consumers/RealtimeNotificationConsumer.cs
+++ src/Taskit.Application/Consumers/RealtimeNotificationConsumer.cs
@@ -1,12 +1,17 @@
 using MassTransit;
+using Microsoft.Extensions.Logging;
 using Taskit.Domain.Events;
 
 namespace Taskit.Application.Consumers;
 
-public class RealtimeNotificationConsumer() : IConsumer<NotificationCreated>
+public class RealtimeNotificationConsumer(ILogger<RealtimeNotificationConsumer> logger) : IConsumer<NotificationCreated>
 {
+    private readonly ILogger<RealtimeNotificationConsumer> _logger = logger;
+
     public Task Consume(ConsumeContext<NotificationCreated> context)
     {
-        throw new NotImplementedException();
+        _logger.LogInformation("Notification created: {NotificationId}",
+            context.Message.NotificationId);
+        return Task.CompletedTask;
     }
 }
